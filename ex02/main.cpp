6b7917d7927fc5cd7359644c5202bfe3fa6ee46d
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-03-18 12:53:35 +0100

    write main with int array and string array, print out normal case and out of bound edge case

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 11:37:44 by ewu               #+#    #+#             */
-/*   Updated: 2025/03/18 12:41:57 by ewu              ###   ########.fr       */
+/*   Updated: 2025/03/18 12:52:09 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,37 @@ int main()
     for (unsigned int i = 0; i < array.size(); ++i)
     {
         array[i] = i % 3;
-        std::cout << "Array at index " << i << "is value: " << array[i] <<'\n';
+        std::cout << "Array at index " << i << " is value: " << array[i] <<'\n';
     }
-
-
+    //out of range test
+    try
+    {
+        std::cout << "Array at index 9 is value: " << array[9] <<'\n';
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    Array<std::string> s(3);
+    // s = {"to", "create", "a string of 3 elements"};
+    s[0] = "to";
+    s[1] = "create";
+    s[2] = "a string of 3 elements";
+    for (unsigned int i = 0; i < s.size(); ++i)
+    {
+        array[i] = i % 3;
+        std::cout << "String at index " << i << " is: " << array[i] <<'\n';
+    }
+    //out of range test
+    try
+    {
+        std::cout << "String at index 5 is value: " << s[5] <<'\n';
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     return 0;
 }
